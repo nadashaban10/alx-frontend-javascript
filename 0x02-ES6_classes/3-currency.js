2a2fad0dfa75d20b900ab6d9910b3d42701fc897
nadashaban10 <nadashaban10@github.com>
2024-05-31 02:51:24 +0300

    ~/alx-frontend-javascript/0x02-ES6_classes$

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+class Currency {
+  constructor(code, name) {
+    if (code && name !== 'string') {
+      throw new TypeError(postMessage);
+    } else {
+      this._code = code;
+      this._name = name;
+    }
+  }
+
+  set code(code) {
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
