d06fa62ba00151662f11f053a89937171b1748bb
nadashaban10 <nadashaban10@github.com>
2024-05-31 02:34:53 +0300

    ~/alx-frontend-javascript/0x02-ES6_classes$

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,43 +1,34 @@
-class HolbertonCourse {
-    constructor(name, length, students) {
-      this.name = name;
-      this.length = length;
-      this.students = students;
-    }
-
-    set name(name) {
-      if (typeof name !== 'string') {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = name;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    set length(length) {
-      if (typeof length !== 'number') {
-        throw new TypeError('Length must be a number');
-      }
-      this._length = length;
-    }
-  
-    get length() {
-      return this._length;
-    }
-
-    set students(students) {
-      if (students instanceof Array) {
-        this._students = students;
-      } else {
-        throw new TypeError('Students must be an Array');
-      }
-    }
-  
-    get students() {
-      return this._students;
-    }
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
   }
-  
-  export default HolbertonCourse;
+
+  set name(name) {
+    if (typeof name === 'string') this._name = name;
+    else throw new TypeError('Name must be a string');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length) {
+    if (typeof length === 'number') this._length = length;
+    else throw new TypeError('Length must be a number');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set students(students) {
+    if (students instanceof Array) this._students = students;
+    else throw new TypeError('Students must be an Array');
+  }
+
+  get students() {
+    return this._students;
+  }
+}
