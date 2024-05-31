009ed1c7f21ddbfcb5acfecbb20c988ebc6a9787
nadashaban10 <nadashaban10@github.com>
2024-05-31 03:50:03 +0300

    ~/alx-frontend-javascript/0x02-ES6_classes$

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,21 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new TypeError('Cannot instantiate from Building directly');
-    }
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
+    if (new.target !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method for evacuationWarningMessage
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
+  set sqft(value) {
+    this._sqft = value;
   }
 }
