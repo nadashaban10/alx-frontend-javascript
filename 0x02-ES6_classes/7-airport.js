9bd2f679ef67850539f88539c099db926229b3f3
nadashaban10 <nadashaban10@github.com>
2024-05-31 03:56:04 +0300

    ~/alx-frontend-javascript/0x02-ES6_classes$

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,4 +1,4 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
@@ -9,4 +9,3 @@ class Airport {
     return `[${this._code}]`;
   }
 }
-
