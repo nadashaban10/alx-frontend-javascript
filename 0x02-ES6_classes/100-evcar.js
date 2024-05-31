0ededb6d5adaccca357cb5399b3b52933ce89d18
nadashaban10 <nadashaban10@github.com>
2024-05-31 04:40:37 +0300

    ~/alx-frontend-javascript/0x02-ES6_classes$

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,6 @@
 import Car from './10-car.js'; // Assuming Car is in 10-car.js
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   static #range = Symbol('range');
 
   constructor(brand, motor, color, range) {
