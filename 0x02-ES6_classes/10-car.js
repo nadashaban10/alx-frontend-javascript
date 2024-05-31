34dcc0522183eb412d1a01049ba2cddefbf3543a
nadashaban10 <nadashaban10@github.com>
2024-05-31 04:19:44 +0300

    ~/alx-frontend-javascript/0x02-ES6_classes$

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,26 +1,41 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof brand !== 'string' || typeof motor !== 'string' || typeof color !== 'string') {
-      throw new TypeError('brand, motor, and color must be strings');
-    }
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
   get brand() {
     return this._brand;
   }
 
+  set brand(value) {
+    this._brand = value;
+  }
+
   get motor() {
     return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
   }
 
   get color() {
     return this._color;
   }
 
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.Allvalues]() {
+    return this;
+  }
+
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    const Allvalues = this.constructor[Symbol.Allvalues];
+
+    return new Allvalues();
   }
 }
